komastudios/android-gamesdk

Add CpuInfo class to discover CPU topology. Set Affinity of the Choreographer thread to little cores.
Choreographer thread running on a Pixel 3:
  - big core @ 825mhz: 0.064ms
  - little core @ 576mhz: 0.171ms

Test: Checked if the thread is running exclusively on the little cores by taking a systrace before and after the patch to compare.

Change-Id: I40b82d08724ed247b78e70ec85389bdf0521e903

src/swappy/src/main/cpp/CpuInfo.h
@@ -1,0 +1,62 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <sched.h>
+
+#include <vector>
+#include <map>
+#include <string>
+
+
+class CpuInfo {
+public:
+    struct Cpu {
+        enum class Type {
+            Little,
+            Big
+        };
+
+        int id;
+        int package_id;
+        long frequency;
+
+        Type type;
+    };
+
+    CpuInfo();
+
+    unsigned int getNumberOfCpus() const;
+
+    const std::vector<Cpu>& cores() const;
+    const std::string hardware() const;
+
+    cpu_set_t littleCoresMask() const;
+    cpu_set_t bigCoresMask() const;
+
+private:
+    std::vector<Cpu> mCpus;
+    std::string mHardware;
+
+    long mMaxFrequency;
+    long mMinFrequency;
+
+    cpu_set_t mLittleCoresMask;
+    cpu_set_t mBigCoresMask;
+};
+
+unsigned int to_mask(cpu_set_t cpu_set);

zoozooll/gamesdk

src/swappy/src/main/cpp/CpuInfo.h
@@ -1,0 +1,68 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <sched.h>
+
+#include <vector>
+#include <map>
+#include <string>
+
+
+class CpuInfo {
+public:
+    struct Cpu {
+        enum class Type {
+            Little,
+            Big
+        };
+
+        int id;
+        int package_id;
+        long frequency;
+
+        Type type;
+    };
+
+    CpuInfo();
+
+    unsigned int getNumberOfCpus() const;
+
+    const std::vector<Cpu>& getCpus() const;
+    const std::string getHardware() const;
+
+    unsigned int getNumberOfLittleCores() const;
+    unsigned int getNumberOfBigCores() const;
+
+    cpu_set_t getLittleCoresMask() const;
+    cpu_set_t getBigCoresMask() const;
+
+private:
+    std::vector<Cpu> mCpus;
+    std::string mHardware;
+
+    long mMaxFrequency;
+    long mMinFrequency;
+
+    unsigned int mNumberOfLittleCores = 0;
+    unsigned int mNumberOfBigCores = 0;
+
+    cpu_set_t mLittleCoresMask;
+    cpu_set_t mBigCoresMask;
+};
+
+unsigned int to_mask(cpu_set_t cpu_set);