3beb9a367dba5c1aa6a088f442acb9edeb07421d
Ady Abraham <adyabr@google.com>
2018-12-20 20:54:48 -0800

    swappy: add stats based on ANDROID_get_frame_timestamps
    Add a feature to Swappy to collect frame statistics.
    
    Test: logcat -s FrameStatistics
    Bug: 122345413
    Change-Id: I4de420ab6ce0c8ac35882c04cecf0e243cb6c701

--- include/swappy/swappy_extra.h
+++ include/swappy/swappy_extra.h
@@ -21,13 +21,17 @@
 #include <EGL/eglext.h>
 #include <jni.h>
 
+#define MAX_FRAME_BUCKETS 6
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// If app is using choreographer and wants to provide choreographer ticks to swappy,
-// call the function below. This function must be called before the first Swappy_swap() call
-// for the first time. Afterwards, call this every choreographer tick
+// If an app wishes to use the Android choreographer to provide ticks to Swappy, it can
+// call the function below.
+// This function *must* be called before the first Swappy_swap() call.
+// Afterwards, call this function every choreographer tick.
 void Swappy_onChoreographer(int64_t frameTimeNanos);
 
 // Pass callbacks to be called each frame to trace execution
@@ -43,15 +47,57 @@ struct SwappyTracer {
 };
 void Swappy_injectTracer(const SwappyTracer *t);
 
-// toggle auto swap interval detection on/off
-// by default, swappy will adjust the swap interval based on actual frame rendering time.
-// App can set this mode to off using this function.
-// If App wants to override the swap interval calculated by swappy it can again to
-// Swappy_setSwapIntervalNS. Swappy will set swap interval to the overridden value and
-// reset its frame timings.
-// NOTE: Swappy may still change this value based on new frames rendering time. To completely
-// override auto swap interval value app needs to call first to Swappy_setAutoSwapInterval(false);
+// Toggle auto-swap interval detection on/off
+// By default, Swappy will adjust the swap interval based on actual frame rendering time.
+// If an app wants to override the swap interval calculated by Swappy, it can call
+// Swappy_setSwapIntervalNS. This will temporarily override Swappy's frame timings but, unless
+// Swappy_setAutoSwapInterval(false) is called, the timings will continue to be be updated
+// dynamically, so the swap interval may change.
 void Swappy_setAutoSwapInterval(bool enabled);
+
+// Toggle statistics collection on/off
+// By default, stats collection is off and there is no overhead related to stats.
+// An app can turn on stats collection by calling Swappy_setStatsMode(true).
+// Then, the app is expected to call Swappy_recordFrameStart for each frame before starting to
+// do any CPU related work.
+// Stats will be logged to logcat with a 'FrameStatistics' tag.
+// An app can get the stats by calling Swappy_getStats.
+void Swappy_enableStats(bool enabled);
+
+struct Swappy_Stats {
+    // total frames swapped by swappy
+    uint64_t totalFrames;
+
+    // Histogram of the number of screen refreshes a frame waited in the compositor queue after
+    // rendering was completed.
+    // for example:
+    //     if a frame waited 2 refresh periods in the compositor queue after rendering was done,
+    //     the frame will be counted in idleFrames[2]
+    uint64_t idleFrames[MAX_FRAME_BUCKETS];
+
+    // Histogram of the number of screen refreshes passed between the requested presentation time
+    // and the actual present time.
+    // for example:
+    //     if a frame was presented 2 refresh periods after the requested timestamp swappy set,
+    //     the frame will be counted in lateFrames[2]
+    uint64_t lateFrames[MAX_FRAME_BUCKETS];
+
+    // Histogram of the number of screen refreshes passed between two consecutive frames
+    // for example:
+    //     if frame N was presented 2 refresh periods after frame N-1
+    //     frame N will be counted in offsetFromPreviousFrame[2]
+    uint64_t offsetFromPreviousFrame[MAX_FRAME_BUCKETS];
+
+    // Histogram of the number of screen refreshes passed between the call to
+    // Swappy_recordFrameStart and the actual present time.
+    //     if a frame was presented 2 refresh periods after the call to Swappy_recordFrameStart
+    //     the frame will be counted in latencyFrames[2]
+    uint64_t latencyFrames[MAX_FRAME_BUCKETS];
+};
+
+void Swappy_recordFrameStart(EGLDisplay display, EGLSurface surface);
+
+void Swappy_getStats(Swappy_Stats *);
 
 #ifdef __cplusplus
 };
