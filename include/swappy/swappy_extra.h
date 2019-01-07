komastudios/android-gamesdk

Add parameters to startFrame and postSwapBuffers tracers, support tracers with different parameters.
Change-Id: I2a59acec830595cc2f9ec45c983d0ad178e61759

include/swappy/swappy_extra.h
@@ -35,8 +35,8 @@ struct SwappyTracer {
     void (*preWait)(void*);
     void (*postWait)(void*);
     void (*preSwapBuffers)(void*);
-    void (*postSwapBuffers)(void*);
-    void (*startFrame)(void*);
+    void (*postSwapBuffers)(void*, long desiredPresentationTime);
+    void (*startFrame)(void*, int currentFrame, long currentFrameTimeStamp);
     void* userData;
 };
 void Swappy_injectTracer(const SwappyTracer *t);

zoozooll/gamesdk

include/swappy/swappy_extra.h
@@ -35,8 +35,8 @@ struct SwappyTracer {
     void (*preWait)(void*);
     void (*postWait)(void*);
     void (*preSwapBuffers)(void*);
-    void (*postSwapBuffers)(void*);
-    void (*startFrame)(void*);
+    void (*postSwapBuffers)(void*, long desiredPresentationTimeMillis);
+    void (*startFrame)(void*, int currentFrame, long currentFrameTimeStampMillis);
     void* userData;
     void (*swapIntervalChanged)(void*);
 